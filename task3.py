6313cfd117da9c368435d8363a584103bfcb7e1b
AlinaBorozenets <115571786+AlinaBorozenets@users.noreply.github.com>
2022-11-03 22:08:44 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,11 +1,14 @@
 def delete(list_, index=None):
-    if index is None:
+    t = len(list_)
 
-        return list_[0:(len(list_) - 1)]
+    if index is None:
+        return list_[0:(t - 1)]
     else:
-        list_ = list_[0:index] + list_[(index+1):len(list_)]
+        list_ = list_[0:index] + list_[(index + 1):t]
         return list_
 
+
+...  # TODO реализовать функцию удаления элемента из списка по индексу
 
 print(delete([0, 0, 1, 2], index=0))  # [0, 1]
 print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
